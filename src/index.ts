12fe160bba6fe51bcd68e332b74ebd9498e49b9b
zhangandi <zhangandi@kuaishou.com>
2020-07-22 21:18:39 +0800

    feat: stash middle2Typestring

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { isObject } from './utils'
 export function transformToTs(json:any) {
-  
+  const json2Middle = transformToMiddleObj(json)
+
 }
 
 type Typeof = "string" | "number" | "bigint" | "boolean" | "symbol" | "undefined" | "object" | "function"
@@ -10,6 +11,20 @@ export interface Middle {
         isRequire: boolean
     } | Middle
 }
+
+interface ResObj { 
+    [key: string]: Middle
+}
+export function middle2TypeString(middle:Middle):string {
+    const res:ResObj  = Object.keys(middle).reduce((resObj:ResObj,curKey:string) => {
+        const curValue = middle[curKey]
+        if(!isObject(curValue)) {
+            resObj[curKey] = <string>middle[curKey].type
+        }
+    },{})
+    return JSON.stringify(res)
+}
+
 export function transformToMiddleObj(json:any,middle:Middle={}):Middle {
     if(isObject(json)) {
         Object.entries(<{[key:string]:any}>json).forEach(([key,value]) => {
