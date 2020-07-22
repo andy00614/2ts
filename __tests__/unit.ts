5946325b6f5bf63e8db95e3309a3fa7457a6c794
zhangandi <zhangandi@kuaishou.com>
2020-07-22 21:18:21 +0800

    feat: stash middle2Typestring

--- __tests__/unit.ts
+++ __tests__/unit.ts
@@ -1,28 +1,51 @@
 import { transformToMiddleObj } from "../src";
 
 describe("通用方法", () => {
+  const simpleMiddleResult = {
+    name: {
+      type: "string",
+      isRequire: true,
+    },
+    age: {
+      type: "number",
+      isRequire: true,
+    },
+    gender: {
+      type: "string",
+      isRequire: true,
+    },
+  };
+
+  const hardMiddleResult = {
+    name: {
+      type: "string",
+      isRequire: true,
+    },
+    age: {
+      type: "number",
+      isRequire: true,
+    },
+    gender: {
+      type: "string",
+      isRequire: true,
+    },
+    info: {
+      a: {
+        type: "number",
+        isRequire: true,
+      },
+      b: { type: "string", isRequire: true },
+    },
+  };
+
   it("转化为中间形式", () => {
     const json = {
       name: "andy",
       age: 25,
       gender: "man",
     };
-    const middleJson = {
-      name: {
-        type: "string",
-        isRequire: true,
-      },
-      age: {
-        type: "number",
-        isRequire: true,
-      },
-      gender: {
-        type: "string",
-        isRequire: true,
-      },
-    };
     const res = transformToMiddleObj(json);
-    expect(res).toStrictEqual(middleJson);
+    expect(res).toStrictEqual(simpleMiddleResult);
   });
 
   it("嵌套对象的转换", () => {
@@ -35,28 +58,10 @@ describe("通用方法", () => {
         b: "2",
       },
     };
-    const middleJson = {
-      name: {
-        type: "string",
-        isRequire: true,
-      },
-      age: {
-        type: "number",
-        isRequire: true,
-      },
-      gender: {
-        type: "string",
-        isRequire: true,
-      },
-      info: {
-        a: {
-          type: "number",
-          isRequire: true,
-        },
-        b: { type: "string", isRequire: true },
-      },
-    };
     const res = transformToMiddleObj(json);
-    expect(res).toStrictEqual(middleJson);
+    expect(res).toStrictEqual(hardMiddleResult);
+  });
+  it("将middle对象转化为字符串", () => {
+    const expect
   });
 });
